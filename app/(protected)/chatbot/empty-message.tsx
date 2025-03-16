91b2ad870d873fbf515e69af0a2beaccc88baef3
Shail Patel <shailmp2009@gmail.com>
2025-03-16 15:56:24 -0400

    Update empty-message text for improved user engagement

--- app/(protected)/chatbot/empty-message.tsx
+++ app/(protected)/chatbot/empty-message.tsx
@@ -7,7 +7,7 @@ const EmptyMessage = () => {
       <MessageSquare className="size-8" />
       <h1 className="text-gray-500 text-xl">No messages yet</h1>
       <p className="text-muted-foreground text-sm">
-        Ask your questions related to health
+        Ask me your questions related to health
       </p>
     </div>
   );
